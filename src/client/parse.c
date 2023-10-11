6b6ea4e4ab579809e6792175e190721668496118
Jonathan <jonno.5000@gmail.com>
2023-10-10 22:39:41 -0400

    Write float coordinates when using an 'extended' game

--- src/client/parse.c
+++ src/client/parse.c
@@ -729,7 +729,7 @@ static void CL_ParseTEntPacket(void)
     case TE_ELECTRIC_SPARKS:
     case TE_BLUEHYPERBLASTER_2:
     case TE_BERSERK_SLAM:
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         MSG_ReadDir(te.dir);
         break;
 
@@ -738,7 +738,7 @@ static void CL_ParseTEntPacket(void)
     case TE_WELDING_SPARKS:
     case TE_TUNNEL_SPARKS:
         te.count = MSG_ReadByte();
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         MSG_ReadDir(te.dir);
         te.color = MSG_ReadByte();
         break;
@@ -751,8 +751,8 @@ static void CL_ParseTEntPacket(void)
     case TE_BUBBLETRAIL2:
     case TE_BFG_LASER:
     case TE_BFG_ZAP:
-        MSG_ReadPos(te.pos1);
-        MSG_ReadPos(te.pos2);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
+        MSG_ReadPos(te.pos2, cl.csr.extended);
         break;
 
     case TE_GRENADE_EXPLOSION:
@@ -776,7 +776,7 @@ static void CL_ParseTEntPacket(void)
     case TE_NUKEBLAST:
     case TE_EXPLOSION1_NL:
     case TE_EXPLOSION2_NL:
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         break;
 
     case TE_PARASITE_ATTACK:
@@ -786,39 +786,39 @@ static void CL_ParseTEntPacket(void)
     case TE_GRAPPLE_CABLE_2:
     case TE_LIGHTNING_BEAM:
         te.entity1 = MSG_ReadShort();
-        MSG_ReadPos(te.pos1);
-        MSG_ReadPos(te.pos2);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
+        MSG_ReadPos(te.pos2, cl.csr.extended);
         break;
 
     case TE_GRAPPLE_CABLE:
         te.entity1 = MSG_ReadShort();
-        MSG_ReadPos(te.pos1);
-        MSG_ReadPos(te.pos2);
-        MSG_ReadPos(te.offset);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
+        MSG_ReadPos(te.pos2, cl.csr.extended);
+        MSG_ReadPos(te.offset, cl.csr.extended);
         break;
 
     case TE_LIGHTNING:
         te.entity1 = MSG_ReadShort();
         te.entity2 = MSG_ReadShort();
-        MSG_ReadPos(te.pos1);
-        MSG_ReadPos(te.pos2);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
+        MSG_ReadPos(te.pos2, cl.csr.extended);
         break;
 
     case TE_FLASHLIGHT:
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         te.entity1 = MSG_ReadShort();
         break;
 
     case TE_FORCEWALL:
-        MSG_ReadPos(te.pos1);
-        MSG_ReadPos(te.pos2);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
+        MSG_ReadPos(te.pos2, cl.csr.extended);
         te.color = MSG_ReadByte();
         break;
 
     case TE_STEAM:
         te.entity1 = MSG_ReadShort();
         te.count = MSG_ReadByte();
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         MSG_ReadDir(te.dir);
         te.color = MSG_ReadByte();
         te.entity2 = MSG_ReadShort();
@@ -829,7 +829,7 @@ static void CL_ParseTEntPacket(void)
 
     case TE_WIDOWBEAMOUT:
         te.entity1 = MSG_ReadShort();
-        MSG_ReadPos(te.pos1);
+        MSG_ReadPos(te.pos1, cl.csr.extended);
         break;
 
     case TE_POWER_SPLASH:
@@ -909,7 +909,7 @@ static void CL_ParseStartSoundPacket(void)
 
     // positioned in space
     if (flags & SND_POS)
-        MSG_ReadPos(snd.pos);
+        MSG_ReadPos(snd.pos, cl.csr.extended);
 
     snd.flags = flags;
 

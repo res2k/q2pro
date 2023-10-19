74c9333324a9a9ce50d8557be04f9a9519a0a9dd
Frank Richter <frank.richter@gmail.com>
2023-10-19 16:30:26 +0200

    Use cg_server_data_t in client_state_t

--- src/client/client.h
+++ src/client/client.h
@@ -22,6 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "shared/shared.h"
 #include "shared/list.h"
+#include "shared/game.h"
 
 #include "common/bsp.h"
 #include "common/cmd.h"
@@ -242,8 +243,7 @@ typedef struct client_state_s {
     //
     // transient data from server
     //
-    char        layout[MAX_NET_STRING];     // general 2D overlay
-    int         inventory[MAX_ITEMS];
+    cg_server_data_t cgame_data;
 
     //
     // server state information

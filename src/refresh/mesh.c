ff94d857e53ce5267019fe2600ab24feb409928f
Jonathan <jonno.5000@gmail.com>
2023-10-18 20:01:40 -0400

    get rebase compiling

--- src/refresh/mesh.c
+++ src/refresh/mesh.c
@@ -29,6 +29,7 @@ static vec3_t   oldscale;
 static vec3_t   newscale;
 static vec3_t   translate;
 static vec_t    shellscale;
+static tessfunc_t tessfunc;
 static vec4_t   color;
 
 static vec3_t   shadedir;
@@ -851,11 +852,6 @@ void GL_DrawAliasModel(const model_t *model)
     setup_shadow();
 
     // select proper tessfunc
-    tessfunc_t tessfunc;
-#if USE_MD5
-    skeltessfunc_t skeltessfunc;
-#endif
-
     if (ent->flags & RF_SHELL_MASK) {
         shellscale = (ent->flags & RF_WEAPONMODEL) ?
             WEAPONSHELL_SCALE : POWERSUIT_SCALE;
@@ -864,17 +860,9 @@ void GL_DrawAliasModel(const model_t *model)
     } else if (dotshading) {
         tessfunc = newframenum == oldframenum ?
             tess_static_shade : tess_lerped_shade;
-#if USE_MD5
-        skeltessfunc = newframenum == oldframenum ?
-            tess_lerped_shade_skel : tess_lerped_shade_skel;
-#endif
     } else {
         tessfunc = newframenum == oldframenum ?
             tess_static_plain : tess_lerped_plain;
-#if USE_MD5
-        skeltessfunc = newframenum == oldframenum ?
-            tess_lerped_plain_skel : tess_lerped_plain_skel;
-#endif
     }
 
     GL_RotateForEntity();

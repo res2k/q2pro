57e3ed69a3c53265060e69374a530e06aa15d32b
Andrey Nazarov <skuller@skuller.net>
2024-04-04 18:50:28 +0300

    Const-ify some function parameters.
    (cherry picked from commit a4b630d67b08274c7c3ea9090ee6613110755524)

--- src/client/main.c
+++ src/client/main.c
@@ -1846,7 +1846,7 @@ typedef struct {
 static list_t   cl_ignore_text;
 static list_t   cl_ignore_nick;
 
-static ignore_t *find_ignore(list_t *list, const char *match)
+static ignore_t *find_ignore(const list_t *list, const char *match)
 {
     ignore_t *ignore;
 
@@ -1859,7 +1859,7 @@ static ignore_t *find_ignore(list_t *list, const char *match)
     return NULL;
 }
 
-static void list_ignores(list_t *list)
+static void list_ignores(const list_t *list)
 {
     ignore_t *ignore;
 

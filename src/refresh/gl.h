36db4ae0397787b0bcbf70b4cbd4880494857c35
Andrey Nazarov <skuller@skuller.net>
2024-08-09 22:23:06 +0300

    Implement distance LOD for MD5 models.
    (cherry picked from commit 1c60845eaad08af65ca2c81965f4a7c73f5ff126)

--- src/refresh/gl.h
+++ src/refresh/gl.h
@@ -228,6 +228,7 @@ extern cvar_t *gl_shaders;
 #if USE_MD5
 extern cvar_t *gl_md5_load;
 extern cvar_t *gl_md5_use;
+extern cvar_t *gl_md5_distance;
 #endif
 extern cvar_t *gl_fog;
 extern cvar_t *gl_damageblend_frac;

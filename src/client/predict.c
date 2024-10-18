57d9f0cc1688031b6773e9fd4dbb8faec1311e82
Frank Richter <frank.richter@gmail.com>
2024-10-18 08:43:04 +0200

    Another CL_CheckPredictionError adjustment

--- src/client/predict.c
+++ src/client/predict.c
@@ -51,7 +51,7 @@ void CL_CheckPredictionError(void)
 
     // save the prediction error for interpolation
     len = fabsf(delta[0]) + fabsf(delta[1]) + fabsf(delta[2]);
-    if (len < 1 || len > 640) {
+    if (len < 1 || len > 80) {
         // > 80 world units is a teleport or something
         VectorClear(cl.prediction_error);
         return;

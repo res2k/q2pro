bbff4d21df42119d656bb81a65860c04344e668f
Andrey Nazarov <skuller32@gmail.com>
2025-04-03 12:41:12 +0300

    Flush SWR after cinematic audio EOF.
    (cherry picked from commit d5f1a3dbf28583a395b1fc9a91b4a2fede2b531f)

--- src/client/ffcin.c
+++ src/client/ffcin.c
@@ -236,7 +236,7 @@ static int process_video(void)
 
 static int process_audio(void)
 {
-    AVFrame *in = cin.frame;
+    AVFrame *in = cin.audio.eof ? NULL : cin.frame;
     AVFrame *out = cin.audio.frame;
     int ret;
 
@@ -247,9 +247,10 @@ static int process_audio(void)
         return ret;
     }
 
-    S_RawSamples(out->nb_samples, out->sample_rate,
-                 av_get_bytes_per_sample(out->format),
-                 out->ch_layout.nb_channels, out->data[0]);
+    if (out->nb_samples)
+        S_RawSamples(out->nb_samples, out->sample_rate,
+                     av_get_bytes_per_sample(out->format),
+                     out->ch_layout.nb_channels, out->data[0]);
     return 0;
 }
 
@@ -272,6 +273,12 @@ static int decode_frames(DecoderState *s)
             Com_DPrintf("%s from %s decoder\n", av_err2str(ret),
                         av_get_media_type_string(dec->codec->type));
             s->eof = true;
+            if (dec->codec->type == AVMEDIA_TYPE_AUDIO) {
+                // flush swr
+                ret = process_audio();
+                if (ret < 0)
+                    return ret;
+            }
             return 0;
         }
 
